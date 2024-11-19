36f2f68f5ad3670e677704ce090d8d540e2da7bc
kartisin <Kartikay.singh@capgemini.com>
2024-11-19 16:50:37 +0530

    modifications in mapstruct for BO and Service layer

--- demoproject/src/main/java/com/example/demo/BO/EmpBOImpl.java
+++ demoproject/src/main/java/com/example/demo/BO/EmpBOImpl.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.EO.EmployeeEO;
-import com.example.demo.MapStruct.EmpMapper;
-import com.example.demo.VO.EmployeeVO;
 import com.example.demo.dao.IEmpRepo;
 import com.example.demo.exception.ResourceNotFoundException;
 
@@ -16,24 +14,20 @@ public class EmpBOImpl implements IEmpBO {
 
 	@Autowired
 	private IEmpRepo empRepo;
-	private EmpMapper empMapper;
+	
 
-	public EmpBOImpl(EmpMapper mapper) {
-		this.empMapper = mapper; // Spring injects the mapper here
-	}
-
-	public EmployeeVO createEmp(EmployeeVO empVo) {
+	@Override
+	public EmployeeEO createEmp(EmployeeEO empEo) {
 		// logger.info("BO layer- creating new Employee");
-		EmployeeEO empEo = empMapper.toEntity(empVo);
-		EmployeeEO savedEmp = empRepo.save(empEo);
-		return empMapper.toVo(savedEmp);
+		return empRepo.save(empEo);
 	}
 
-	public EmployeeVO retrieveData(int id) throws ResourceNotFoundException {
+	@Override
+	public EmployeeEO retrieveData(int id) throws ResourceNotFoundException {
 		Optional<EmployeeEO> empEo = empRepo.findById(id);
 		if (empEo.isPresent()) {
 			// logger.info("Retrieving Employee with Id :", id);
-			return empMapper.toVo(empEo.get());
+			return empEo.get();
 		} else
 			throw new ResourceNotFoundException("Record not found");
 	}
--- demoproject/src/main/java/com/example/demo/BO/IEmpBO.java
+++ demoproject/src/main/java/com/example/demo/BO/IEmpBO.java
@@ -1,5 +1,15 @@
 package com.example.demo.BO;
 
+import com.example.demo.EO.EmployeeEO;
+import com.example.demo.MapStruct.EmpMapper;
+import com.example.demo.VO.EmployeeVO;
+import com.example.demo.exception.ResourceNotFoundException;
+
 public interface IEmpBO {
 
+	 
+	public EmployeeEO createEmp(EmployeeEO empEo);
+	 EmployeeEO retrieveData(int id) throws ResourceNotFoundException;
+	 
+	
 }
