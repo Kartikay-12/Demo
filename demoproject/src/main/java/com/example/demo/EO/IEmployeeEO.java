1fe86bf20a37358d28bfd17de6e3f989cb90c9e6
kartisin <Kartikay.singh@capgemini.com>
2024-11-25 17:04:48 +0530

    chanfed done in EO layer mockito working for BO layer

--- demoproject/src/main/java/com/example/demo/EO/IEmployeeEO.java
+++ demoproject/src/main/java/com/example/demo/EO/IEmployeeEO.java
@@ -1,13 +1,14 @@
 package com.example.demo.EO;
 
 import com.example.demo.DTO.EmployeeDto;
+import com.example.demo.exception.ResourceNotFoundException;
 
 
 public interface IEmployeeEO {
 
 	EmployeeDto createEmp(EmployeeDto save);
 
-	EmployeeDto retrieveData(EmployeeDto employeeDto);
+	EmployeeDto retrieveData(int id) throws ResourceNotFoundException;
 	
 
 }
